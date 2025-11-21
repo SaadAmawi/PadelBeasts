19d15008b1f534a8ac58291294268f4e5fc3702a
Saad Amawi <saadamawi@yahoo.com>
2025-11-22 01:52:13 +0400

    hero and header done as well as coaches and about us

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import GlassSurface from '../../ReactBits/GlassSurface/GlassSurface';
+import image from '../../assets/images/logo 2.png'
 import './index.css'
 function Header() {
   return (
@@ -8,11 +9,17 @@ function Header() {
      width="70%"
      > 
      <div className='glass'>
+    <div className='logo'>
+    <img src={image} width={20} height={60}/>
+    </div>
+    <div className='buttons'>
      <button className='header-button'>Home</button>
      <button className='header-button'>About Us</button>
      <button className='header-button'>Coaches</button>
      <button className='header-button'>Services</button>
      <button className='header-button'>Contact Us</button>
+     </div>
+     <div className='end'></div>
      </div>
      </GlassSurface>
     </div>
