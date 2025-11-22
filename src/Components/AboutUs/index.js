ab3aad08fec235928abffea2e5637b5e889627ba
Saad Amawi <saadamawi@yahoo.com>
2025-11-22 15:26:34 +0400

    adding some styling, looks like shit currently

--- src/Components/AboutUs/index.js
+++ src/Components/AboutUs/index.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './index.css'
-import LiquidEther from '../../ReactBits/LiquidEther/LiquidEther'
+import padel from '../../assets/images/padel2.png'
 function About() {
+
   return (
     <div className="about-container">
 
-
+      <img src={padel} className='abs'/>
+      <div className='left'>
       <div className="text">
-          <h1 className='Title'>Building a Strong Padel Community in UAE</h1>
+          <h1 className='Title'>Building a Strong Padel <span className='color'>Community</span> in UAE</h1>
           <h2 className='about'>
            Padel Beast is dedicated to fostering the growth of the padel tennis in the United Arab Emirates. With a passion for the game and a commitment to excellence, we offer a comprehensive range of services including coaching lessons, match making, tournaments, club management, and more. Our mission is to create a thriving padel community that brings people together, promotes competition, and provides opportunities for players of all levels to improve their skills.
           </h2>
       </div>
+      </div>
+      <div className='right'>
+      {/* <h1 className='Title'><span className='color2'>Padel Beast TV</span></h1> */}
+          <iframe src='https://www.youtube.com/embed/ocl3bBhTpVc'
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded YouTube Video"/>
+            </div>
 </div>
 
 
--- src/Components/Coaches/index.js
+++ src/Components/Coaches/index.js
@@ -10,8 +10,10 @@ import lomp from '../../assets/images/lop.jpg'
 import Iris from '../../assets/images/coach2.png'
 function Coaches() {
   return (
-    <div className='Coaches'>
+    <div className='Coaches-container'>
+      <div className='content'>
         <h1 className='title'>Our Coaches</h1>
+        <p className='texts'>Our experienced coaches provide personalized one-on-one lessons as well as group lessons for two or more players to improve your padel tennis skills and enhance your game. Whether you're a beginner or an advanced player, we have the expertise to help you reach your full potential.</p>
         <div className='coaches'>
         <CoachCard Name={"Navid Fahadezh"} Image={navaro}/>
         <CoachCard Name={"Cristina Navaro"} Image={cristina}/>
@@ -19,6 +21,7 @@ function Coaches() {
         <CoachCard Name={"Marc Llompart"} Image={lomp}/>
         <CoachCard Name={"Iris Murcia"} Image={Iris}/>
         <CoachCard Name={"bernardo konzen"} Image={bern}/>
+        </div>
         </div>
     </div>
   )
